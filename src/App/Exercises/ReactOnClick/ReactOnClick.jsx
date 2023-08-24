24dc4f1450f4fb27b40b847e8f769f330fc9212d
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-08-24 20:55:18 +0200

    input sending value to paragraph below

--- src/App/Exercises/ReactOnClick/ReactOnClick.jsx
+++ src/App/Exercises/ReactOnClick/ReactOnClick.jsx
@@ -2,20 +2,26 @@ import { useState } from 'react';
 import './styles.css';
 
 export const ReactOnClick = () => {
-  const [count, setCount] = useState(0);
-
+  //const [count, setCount] = useState(0);
+  const [value, setValue] = useState('');
   // const text = isClicked ? 'Kliknięcie' : 'Nie kliknięte';
 
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
   function handleClick() {
-    setCount(count + 1);
+    setValue('');
   }
 
   return (
     <div>
       <h1>Cześć!</h1>
-      <button id="txtA" onClick={handleClick}>
-        {count}
-      </button>
+      <button onClick={handleClick}>Resetuj</button>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>
+        <strong>{value}</strong>
+      </p>
     </div>
   );
 };
