249bd15d69ec4456a22d00b0ad9b5350b680c29f
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-08-28 20:46:46 +0200

    Added styling into subroute

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/SelectorsAndCascade.jsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+export const SelectorsAndCascade = () => {
+  return (
+    <>
+      <div className="test">
+        <span>Pierwszy</span>
+        <span>Drugi</span>
+        <span>Trzeci</span>
+        <span>Czwarty</span>
+        <span>Piąty</span>
+        <span>Szósty</span>
+      </div>
+      <button className="hover">Kliknij</button>
+      <input type="text" className="firstname" />
+      <div className="pseudo">Pseudo</div>
+    </>
+  );
+};
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorsAndCascade } from './SelectorsAndCascade';
+
+export const htmlAndCssRouterMetaData = {
+  path: 'selectors-and-cascade',
+  date: '28-08-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie z selektorami i kaskadą',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['selectors', 'class', 'atribute'],
+};
