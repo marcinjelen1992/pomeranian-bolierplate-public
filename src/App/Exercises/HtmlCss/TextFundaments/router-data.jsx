c8bbb946a25657776505667a1069fc9148d881f8
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-08-29 19:00:12 +0200

    Text styling example subroute

---
+++ src/App/Exercises/HtmlCss/TextFundaments/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { TextFundaments } from './TextFundaments';
+
+export const textFundamentsRouterMetaData = {
+  path: 'text-fundaments-css',
+  date: '29-08-2023',
+  linkLabel: 'Praca z tekstem',
+  blockNo: 1,
+  element: <TextFundaments />,
+  tags: ['selectors', 'class', 'atribute'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { htmlAndCssRouterMetaData as htmlAndCssRouterMetaData } from './SelectorsAndCascade/router-data';
+import { textFundamentsRouterMetaData as textFundamentsRouterMetaData } from './TextFundaments/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   htmlAndCssRouterMetaData,
+  textFundamentsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
