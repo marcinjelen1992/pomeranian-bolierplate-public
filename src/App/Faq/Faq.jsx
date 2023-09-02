4227c6e3a548fd44c1484512db81042f7e82a5c0
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-09-02 23:11:05 +0200

    Made FAQ with answer section collapsing by clicking question section

--- src/App/Faq/Faq.jsx
+++ src/App/Faq/Faq.jsx
@@ -6,18 +6,39 @@ export const Faq = () => {
     <div className="faq-main">
       <strong>FAQ</strong>
       <p>Tutaj znajdziesz odpowiedzi na najczęściej zadawane pytania</p>
-      <SingleQuestion question="Jak masz na imię?" answer="Marcin" />
+      <SingleQuestion
+        question="Jak masz na imię? (kliknij żeby rozwinąć)"
+        answer="Marcin"
+      />
       <SingleQuestion question="Ulubiony kolor" answer="fioletowy" />
       <SingleQuestion question="Ulubiona potrawa" answer="kurczak kung-pao" />
-      <SingleQuestion />
-      <SingleQuestion />
-      <SingleQuestion />
-      <SingleQuestion />
-      <SingleQuestion />
-      <SingleQuestion />
       <SingleQuestion
         question="Czy jestem polem opartym na propsie?"
         answer="Tak, jestem oparty na propsie"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
+      />
+      <SingleQuestion
+        question="Czy mam Loremx25 jako odpowiedź?"
+        answer="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid tempore debitis cum harum molestiae quam, quae dignissimos, ea dolores quos fugit corrupti consequuntur fuga beatae?"
       />
     </div>
   );
--- src/App/Faq/SingleQuestion.jsx
+++ src/App/Faq/SingleQuestion.jsx
@@ -1,11 +1,17 @@
 import './styles.css';
+import { useState } from 'react';
 
 export const SingleQuestion = ({ question, answer }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  function handleClick() {
+    setIsVisible(!isVisible);
+  }
+
   return (
     <div>
       <div className="faq-list">
-        <p>{question}</p>
-        <p>{answer}</p>
+        <p onClick={handleClick}>{question}</p>
+        {isVisible && <p>{answer}</p>}
       </div>
     </div>
   );
