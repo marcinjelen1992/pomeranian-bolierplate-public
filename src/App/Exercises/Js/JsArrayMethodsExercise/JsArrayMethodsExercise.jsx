abe9cfe6994d33d75875a88b22b32c00a60d5ace
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-09-12 23:51:41 +0200

    Modified JsArrayMethodsExercise with many different things

--- src/App/Exercises/Js/JsArrayMethodsExercise/JsArrayMethodsExercise.jsx
+++ src/App/Exercises/Js/JsArrayMethodsExercise/JsArrayMethodsExercise.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './styles.css';
 
 export const JsArrayMethodsExercise = () => {
@@ -8,6 +9,8 @@ export const JsArrayMethodsExercise = () => {
   const myArrayA = [1, 2, 3, 4, 5];
   const myArrayB = [3, 5, 7, 9];
   const myArrayC = [1, 2, 3, 4, 5, 6];
+  const myArrayD = [1, 3, 5, 7, 9];
+  const myArrayE = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   function sameNumbers(array1, array2) {
     const commonElements = array1.filter((item) => array2.includes(item));
@@ -44,12 +47,158 @@ export const JsArrayMethodsExercise = () => {
   // create2dArray(1,2) => [[0,0]]
   // create2dArray(2,2) => [[0,0],[0,0]]
 
+  function reverseArray(array) {
+    const mappedValue = array.map((value) => value);
+    const reversed = mappedValue.reverse();
+    return reversed;
+  }
+
+  function alternativeReverseArray(array) {
+    const newArray = array.reverse((value) => value);
+    return newArray;
+  }
+
+  console.log(reverseArray(myArrayA));
+  console.log(typeof reverseArray(myArrayA));
+  console.log(reverseArray(myArrayB));
+  console.log(reverseArray(myArrayC));
+  console.log(alternativeReverseArray(myArrayC));
+
+  const object = [
+    { id: 1, name: 'John', age: 20 },
+    { id: 2, name: 'Adam', age: 24 },
+    { id: 3, name: 'Emily', age: 30 },
+  ];
+
+  function getNames(obj) {
+    return obj.map(({ name }) => name);
+  }
+  console.log(getNames(object));
+
+  function sumOfAge(obj) {
+    return obj.reduce(
+      (accumulator, currentValue) => accumulator + currentValue.age,
+      0
+    );
+  }
+  console.log(sumOfAge(object));
+
+  const [anagramValueA, setAnagramValueA] = useState('');
+  const [anagramValueB, setAnagramValueB] = useState('');
+
+  function GetAnagramA(event) {
+    setAnagramValueA(event.target.value);
+  }
+
+  function GetAnagramB(event) {
+    setAnagramValueB(event.target.value);
+  }
+
+  function isAnagram(firstWord, secondWord) {
+    const firstWordArray = firstWord.split('').sort();
+    const secondWordArray = secondWord.split('').sort();
+
+    return firstWordArray.toString() === secondWordArray.toString()
+      ? true
+      : false;
+  }
+
+  function commonLetters(str1, str2) {
+    const letters1 = new Set(str1);
+    const arrayLetters2 = Array.from(new Set(str2));
+    return Array.from(letters1).filter((letter) =>
+      arrayLetters2.includes(letter)
+    );
+  }
+  console.log('commonLetters', commonLetters('Hello', 'World'));
+
+  const [stringA, setStringA] = useState('');
+  const [stringB, setStringB] = useState('');
+  const [stringC, setStringC] = useState('');
+
+  function GetStringA(event) {
+    setStringA(event.target.value);
+  }
+
+  function GetStringB(event) {
+    setStringB(event.target.value);
+  }
+
+  function GetStringC(event) {
+    setStringC(event.target.value);
+  }
+
+  function toCamelCase(str) {
+    const words = str.split(/[ \-_]/);
+    const camelCaseWords = words.map((word, index) => {
+      if (index === 0) {
+        return word.toLowerCase();
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      }
+    });
+    return camelCaseWords.join('');
+  }
+  console.log(toCamelCase('Hello world'));
+
+  function isEven(array1, array2) {
+    const commonElements = array1.filter((item) => !array2.includes(item));
+    return commonElements;
+  }
+
+  console.log(isEven(myArrayE, myArrayD));
+
+  function doubleCharacter(str) {
+    const words = str.split(/[ \-_]/);
+    const camelCaseWords = words.map((word, index) => {
+      if (index === 0) {
+        return word.toLowerCase();
+      } else {
+        return word.charAt(0).toUpperCase() + word.charAt(0);
+      }
+    });
+    return camelCaseWords.join('');
+  }
+
+  console.log(doubleCharacter('Hello world'));
+
+  // zdublowało w ze słowa world. Czy użyć slice? Czy użyć iterację?
+
   return (
     <div className="js-array-methods-exercises-page">
       <h3>Hello there</h3>
       <p>{sameNumbers(myArrayA, myArrayB)}</p>
       <p>{averageNumbers(myArrayC)}</p>
       <p>{create2dArray(2, 2)}</p>
+      <p>{reverseArray(myArrayA)}</p>
+      <ul>
+        {object.map((item) => {
+          return (
+            <li key={item.id} id={item.id}>{`${item.name} ${item.age}`}</li>
+          );
+        })}
+      </ul>
+      <p>{getNames(object)}</p>
+      <p>{sumOfAge(object)}</p>
+      <h3>Sprawdź czy dwa wpisane słowa są anagramami</h3>
+      <p>
+        <input type="text" value={anagramValueA} onChange={GetAnagramA} />
+        <input type="text" value={anagramValueB} onChange={GetAnagramB} />
+        {isAnagram(anagramValueA, anagramValueB)
+          ? 'Jest to anagram'
+          : 'Nie jest to anagram'}
+      </p>
+      <h3>Wspólne znaki dla dwóch wyrazów</h3>
+      <p>
+        <input type="text" value={stringA} onChange={GetStringA} />
+        <input type="text" value={stringB} onChange={GetStringB} />
+        {commonLetters(stringA, stringB)}
+      </p>
+      <h3>Konwersja do camelCase</h3>
+      <p>
+        <input type="text" value={stringC} onChange={GetStringC} />
+        {toCamelCase(stringC)}
+      </p>
     </div>
   );
 };
