e0c0f4c3dfa236361d45fcd41101212bdea12c15
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-09-18 23:43:07 +0200

    Stopped at dead end for individual button color change but at least it doesnt crash

--- src/App/Exercises/Js/HitTheMoleGameProper/HitTheMoleGameProper.jsx
+++ src/App/Exercises/Js/HitTheMoleGameProper/HitTheMoleGameProper.jsx
@@ -2,10 +2,66 @@ import './styles.css';
 import { useState, useEffect } from 'react';
 
 export const HitTheMoleGameProper = () => {
+  const DEFAULT_MOLE_GAME = [
+    { name: 'oneminute', mole: 'onemole', isHighlighted: false },
+    { name: 'twominutes', mole: 'twomoles', isHighlighted: false },
+    { name: 'threeminutes', mole: 'threemoles', isHighlighted: false },
+  ];
+
+  const [items, setItems] = useState(DEFAULT_MOLE_GAME);
+  console.log(items);
+
+  //  const handleClick = (event) => {
+  //    setItems(
+  //      items.map((item) => {
+  //        if (item.time === event.target.id) {
+  //          return { ...item, isHighlighted: true };
+  //        } else {
+  //          return item;
+  //       }
+  //      })
+  //    );
+  //  };
+
+  //  const handleClick = (event) => {
+  //    setItemTime((prev) => ({
+  //      ...prev,
+  //      [event.target.name]: event.target.value,
+  //    }));
+  //  };
+
   const [isChangeBckgr, setIsChangeBckgr] = useState(false);
   function handleClick() {
     setIsChangeBckgr(!isChangeBckgr);
   }
+
+  const handleClickMap = (event) => {
+    setItems(
+      items.map((item) => {
+        if (item.name === event.target.id) {
+          return { ...item, isHighlighted: true };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+
+  function getNames(obj) {
+    return obj.map(({ isHighlighted }) => isHighlighted);
+  }
+  console.log(getNames(items));
+
+  // const handleClick = (event) => {
+  //  setItems((item) => {
+  //      if (item.name === event.target.id) {
+  //        return {setIsChangeBckgr: true };
+  //     } else {
+  //       return setIsChangeBckgr: false;
+  //      }
+  //    })
+  //  ;
+  //};
 
   return (
     <div className="molegamewrapper">
@@ -23,7 +79,20 @@ export const HitTheMoleGameProper = () => {
         </div>
         <div className="playingboard">
           <div>
+            {items.map((item) => (
+              <button
+                key={item.name}
+                data-highlighted={item.isHighlighted}
+                id={item.name}
+                onClick={handleClickMap}
+              >
+                {item.name}
+              </button>
+            ))}
             <button
+              key={DEFAULT_MOLE_GAME.oneminute}
+              data-highlighted={DEFAULT_MOLE_GAME.isHighlighted}
+              id={DEFAULT_MOLE_GAME.oneminute}
               className={isChangeBckgr ? 'button_nr2' : 'button_nr1'}
               onClick={handleClick}
             >
