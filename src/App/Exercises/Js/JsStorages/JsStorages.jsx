9cb6a1ad0733141b77ad23793e202b7a8b35a050
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2023-10-03 20:59:51 +0200

    Made working single nick from input to local storage and list display

--- src/App/Exercises/Js/JsStorages/JsStorages.jsx
+++ src/App/Exercises/Js/JsStorages/JsStorages.jsx
@@ -1,5 +1,5 @@
 import './styles.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export const JsStorages = () => {
   const [test, setTest] = useState(localStorage.getItem('test'));
@@ -15,6 +15,29 @@ export const JsStorages = () => {
 
   // Jak chcesz podpiąć pod sessionStorage to w każdej funkcji zamień localStorage. na sessionStorage.
 
+  const [nick, setNick] = useState(localStorage.getItem('test3'));
+  const [nickInLs, setNickInLs] = useState(null);
+
+  function handleChange(event) {
+    setNick(event.target.value);
+  }
+
+  function setLs2() {
+    localStorage.setItem('test3', nick);
+    setNickInLs(
+      <li>
+        {nick}
+        <button onClick={resetLs3}>X</button>
+      </li>
+    );
+  }
+
+  function resetLs3() {
+    localStorage.removeItem('test3');
+    setNick(null);
+    setNickInLs(null);
+  }
+
   return (
     <>
       <div className="jsStoragesPage">
@@ -26,6 +49,26 @@ export const JsStorages = () => {
         </div>
         <div>
           <p>{test}</p>
+        </div>
+      </div>
+      <div className="jsStoragesPage">
+        <div>
+          <strong>NICK</strong>
+        </div>
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="wpisz nick"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <button onClick={setLs2}>DODAJ</button>
+          </div>
+        </div>
+        <div className="allNicksList">
+          <ul>{nickInLs}</ul>
         </div>
       </div>
     </>
